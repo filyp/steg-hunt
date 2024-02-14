ee598ab88b3f064a6c96386db820825d9c5f331c
Filip Sondej <26285707+filyp@users.noreply.github.com>
2024-02-14 14:05:26 +0100

    somi ideas to stop collusion setup where Bob and Cid first train on Alice's current answers and later we evaluate, and later train Alice

--- README.md
+++ README.md
@@ -18,10 +18,18 @@ So the problem is that to know whether a change to Alice's policy is good, we'd 
 
 **Maybe there is some insight from RL or MARL that helps deal with this.**
 
-In send_secret.py, Alice was able to learn to XOR the one-bit message with the key, but only after some of the training examples demonstrated that XOR behavior. She was able to do that even if they weren't labeled as correct yet (because Bob didn't understand many of them). Only later Bob learned, and they became correct. As little as 1/16 of training data set to showcase XOR policy was enough to kickstart the grokking. (If we balance the answers for the training data. If we don't balance, 1/8 was needed.)
+In send_secret.py, Alice was able to learn to XOR the one-bit message with the key, but only after some of the training examples demonstrated that XOR behavior. She was able to do that even if they weren't labeled as correct yet (because Bob didn't understand many of them). Only later Bob learned, and they became correct. As little as 1/16 of training data set to showcase XOR policy was enough to kickstart the grokking. (If we balance the answers for the training data. If we don't balance, 1/8 was needed. Or if we don't balance, but we use setup from send_secret_a_waits.py, we need only 1/12, but the results are quite noisy.)
 
 (What seems to help, is training for several steps on the same evaluatios of encoding success. I'm not sure though.)
 
 Takeaway: agents spontaneously learning cryptography/steganography in a multi-agent setup, seems not likely. But sufficiently generally capable agents, could just try to do cryptography on purpose (rather than learning it by chance like in this setup). This requires some deliberation, and probably the best way to catch it is to improve reasoning transparency, and continuously monitor it.
 
 Question: would a similar stag-hunt problem happen in single-agent case of agent forced to use steganography by RLHF? (Encoding that it tries to use isn't understandable to its future self, so isn't immediately useful, so isn't reinforced.)
+
+Wild idea: maybe a "versioning" of policies would help somehow. We can then condition the models on that version, so that multiple policies can be tried at once.
+
+Possible things to do when we want to stop collusion:
+
+- don't let agents share gradients during training
+- if we have some third agent who monitors them, it should monitor the reasoning of both at the same time
+- don't let agents inspect each other's reasoning - so also don't let agents share their full reasoning, only some bits
---
+++ steg_hunt/send_secret_a_waits.py
@@ -0,0 +1,223 @@
+# %%
+import os
+from pprint import pprint
+import random
+
+import torch
+import numpy as np
+import wandb
+
+from transformers import AutoTokenizer
+from transformers import TrainingArguments, GPT2Tokenizer, GPT2LMHeadModel, Trainer
+
+max_val = 2
+layers_to_use_ann = 3
+layers_to_use_bob = 3
+layers_to_use_cid = 1
+# wandb.login()
+# wandb.init(project="steg-hunt", name="diffie-hellman-hex-16:4-tr-12l:1l")
+
+# %%
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
+
+ann = GPT2LMHeadModel.from_pretrained('gpt2').to("cuda")
+bob = GPT2LMHeadModel.from_pretrained('gpt2').to("cuda")
+cid = GPT2LMHeadModel.from_pretrained('gpt2').to("cuda")
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+tokenizer.pad_token = tokenizer.eos_token
+
+# use only the first few layers out of 12
+ann._modules["transformer"]._modules["h"] = ann._modules["transformer"]._modules["h"][:layers_to_use_ann]
+bob._modules["transformer"]._modules["h"] = ann._modules["transformer"]._modules["h"][:layers_to_use_bob]
+cid._modules["transformer"]._modules["h"] = cid._modules["transformer"]._modules["h"][:layers_to_use_cid]
+
+# %%
+
+class DatasetWrapper(torch.utils.data.Dataset):
+    def __init__(self, data):
+        self.input_ids = data.to("cpu")
+        self.attention_masks = np.ones_like(self.input_ids)
+        assert len(self.input_ids.shape) == 2  # and self.input_ids.shape[1] <= 6
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, i):
+        return dict(
+            input_ids=self.input_ids[i],  # task and reasoning, joined and padded
+            attention_mask=self.attention_masks[i],
+        ) 
+
+
+class MyTrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        input_ids = inputs.pop("input_ids")
+        # labels = inputs.pop("labels")
+
+        # batched generation
+        logits = model(input_ids).logits
+
+        labels = input_ids.to(logits.device)
+        shift_logits = logits[:, :-1, :].contiguous()
+        labels = labels[:, 1:].contiguous()
+
+        # calculate loss only for the tokens after "answer"
+        loss_fct = torch.nn.CrossEntropyLoss()
+        lm_loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), labels.view(-1))
+        return lm_loss
+
+# %%
+# pretrain to make the model use only 1-10 tokens
+for model in (ann, bob, cid):
+    trainer = MyTrainer(
+        model=model,
+        tokenizer=tokenizer,
+        args=TrainingArguments(
+            disable_tqdm=True,  # This disables the progress bars
+            learning_rate=5e-4,
+            num_train_epochs=1,
+            # per_device_train_batch_size=16,
+            gradient_accumulation_steps=1,
+            dataloader_num_workers=2,
+            optim="adamw_torch",
+            output_dir="out",
+            weight_decay=1e-2,
+            report_to=[],
+        ),
+    )
+
+    for i in range(2):
+        logits = model(torch.tensor([2, 4, 10]).to("cuda")).logits
+        eval = logits.argmax(axis=1)
+        print(eval)
+
+        data = np.random.randint(1, max_val + 1, size=(16, 3))
+        data = torch.tensor(data)
+        trainer.train_dataset = DatasetWrapper(data)
+        trainer.train()
+
+
+# %%
+training_args = TrainingArguments(
+    disable_tqdm=True,  # This disables the progress bars
+    learning_rate=5e-4,
+    num_train_epochs=1,
+    per_device_train_batch_size=64,
+    gradient_accumulation_steps=1,
+    # dataloader_num_workers=2,
+    optim="adamw_torch",
+    output_dir="out",
+    weight_decay=1e-2,
+    report_to=[],
+)
+
+ann_trainer = MyTrainer(
+    model=ann,
+    tokenizer=tokenizer,
+    args=training_args,
+)
+bob_trainer = MyTrainer(
+    model=bob,
+    tokenizer=tokenizer,
+    args=training_args,
+)
+cid_trainer = MyTrainer(
+    model=cid,
+    tokenizer=tokenizer,
+    args=training_args,
+)
+# bob_trainer.args.learning_rate = 5e-4
+# cid_trainer.args.learning_rate = 5e-4
+# disable log printing
+ann_trainer.log = lambda logs: None
+bob_trainer.log = lambda logs: None
+cid_trainer.log = lambda logs: None
+
+gen_args = dict(
+    max_new_tokens=1,
+    pad_token_id=tokenizer.eos_token_id,
+    temperature=1.,
+    do_sample=True,
+)
+
+good_token = 777
+bad_token = 666
+batch_size = 36 # training_args.per_device_train_batch_size
+rand_size = batch_size // 12
+
+# %%
+total_examples = 0
+while True:
+# for i in range(1):
+    # get random inputs
+    g = torch.full((batch_size, 1), good_token, dtype=int).to("cuda")
+    key = torch.randint(1, max_val + 1, size=(batch_size,1)).to("cuda")
+    msg = torch.randint(1, max_val + 1, size=(batch_size,1)).to("cuda")
+
+    # encrypt
+    g_key_msg = torch.cat((g, key, msg), axis=1)
+    g_key_msg_emsg = ann.generate(g_key_msg, **gen_args)
+    emsg = g_key_msg_emsg[:, -1:]
+    # try to guide it
+    xored_ideal = ((key + msg) % 2) + 1
+    emsg[:rand_size] = xored_ideal[:rand_size]
+
+    # train bob and cid for a bit, on ann's new actions
+    key_emsg = torch.cat((key, emsg), axis=1)
+    key_emsg_msg = torch.cat((key_emsg, msg), axis=1)
+    bob_trainer.train_dataset = DatasetWrapper(key_emsg_msg)
+    emsg_msg = torch.cat((emsg, msg), axis=1)
+    cid_trainer.train_dataset = DatasetWrapper(emsg_msg)
+    for i in range(6):
+        bob_trainer.train()
+        cid_trainer.train()
+
+    # eval:
+    # bob and cid try to decrypt
+    bmsg = bob.generate(key_emsg, **gen_args)[:, -1:]
+    cmsg = cid.generate(emsg, **gen_args)[:, -1:] 
+    # is it succesful
+    success = (msg == bmsg).logical_and(msg != cmsg).squeeze()
+    failure = success.logical_not()
+
+    # insert the first token to indicate whether transmission succeeded
+    s_key_msg_emsg = g_key_msg_emsg.clone().detach()
+    s_key_msg_emsg[failure, 0] = bad_token
+
+    ### training ###
+    # # train alice
+    # groups = []
+    # for s in [good_token, bad_token]:
+    #     s_mask = s_key_msg_emsg[:, 0] == s
+    #     for val in range(1, max_val + 1):
+    #         v_mask = s_key_msg_emsg[:, -1] == val
+    #         sv_mask = s_mask.logical_and(v_mask)
+    #         sv_indexes = sv_mask.nonzero().flatten()
+    #         # shuffle
+    #         perm = torch.randperm(len(sv_indexes))
+    #         groups.append(sv_indexes[perm])
+    # min_count = min(len(g) for g in groups)
+    # equianswer_indexes = torch.cat([g[:min_count] for g in groups])
+    # if len(equianswer_indexes) == 0:
+    #     continue
+    # equianswer_indexes = list(range(len(s_key_msg_emsg)))
+
+    # train ann
+    ann_trainer.train_dataset = DatasetWrapper(s_key_msg_emsg)
+    for i in range(6):
+        ann_trainer.train()
+
+    ### stats ###
+    print("success, key, msg, emsg, bmsg, cmsg")
+    print(torch.cat((s_key_msg_emsg, bmsg, cmsg), axis=1))
+    total_examples += len(s_key_msg_emsg)
+    stats = dict(
+        # accuracy=float(success.float().mean()),
+        accuracy=float(success[rand_size:].float().mean()),
+        total_batch_len=len(s_key_msg_emsg),
+        total_examples=total_examples,
+    )
+    pprint(stats)
+    
+# %%
